7af6d2a62a37cd7b49508a2f9da75624aa42c68c
saranyashanmugam04 <37809422+saranyashanmugam04@users.noreply.github.com>
2019-09-26 11:02:07 +0530

    updating cucumber prject 2.0

--- src/test/java/skeleton/Runner.java
+++ src/test/java/skeleton/Runner.java
@@ -6,8 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\skeleton\\login.feature",
-				plugin="json:target/cucumberreport.json")
+@CucumberOptions(plugin="json:target/cucumberreport.json")
 				//plugin="html:target/htmlreport")
 public class Runner {
 
